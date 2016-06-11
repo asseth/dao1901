b98c39a565a621a5845abc1c088e8c25d31a9d3f
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-06-11 19:36:44 +0200

    add jquery

--- truffle/app/javascripts/app.js
+++ truffle/app/javascripts/app.js
@@ -1,7 +1,12 @@
-function test(message) {
-  var president = document.getElementById("president").value;
-  var tresorier = document.getElementById("tresorier").value;
-  var secretaire = document.getElementById("secretaire").value;
-  console.log(president, tresorier, secretaire);
-  status.innerHTML = message;
-};
+$(function () {
+  $('#deploy').click(function() {
+    var president = document.getElementById("president").value;
+    var tresorier = document.getElementById("tresorier").value;
+    var secretaire = document.getElementById("secretaire").value;
+    console.log(president, tresorier, secretaire);
+    // TODO : déployer le contrat avec les membres du bureau
+    // TODO : ouvrir page
+  });
+});
+
+
