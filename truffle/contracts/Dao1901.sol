e0dfb6580e440246d263c207b1f8dbe6ed4c781a
Altais <cervantescedric@gmail.com>
2016-06-11 16:49:53 +0200

    fix dao

--- truffle/contracts/Dao1901.sol
+++ truffle/contracts/Dao1901.sol
@@ -1,29 +1,55 @@
 contract Dao1901 {
-    
-    enum RoleList { secretary, president, treasurer, member } // Role Available in the DAO
+
+    enum RoleList { founder, secretary, president, treasurer, member } // Role Available in the DAO
 
     struct Member {
         address key;
-        bool active; // MONEY OR NOT !!!
+        bool payed;
+        bool canVote;
         RoleList role;
     }
-    
+
     mapping(address => Member) members;
-    
+
+    bool created;
+
     modifier isRole(RoleList role) {
         if (members[msg.sender].role != role) throw;
-        _
     }
-    
+    modifier isAdmin() {
+        if (members[msg.sender].role != RoleList.founder ||
+            members[msg.sender].role != RoleList.secretary || 
+            members[msg.sender].role != RoleList.president ||
+            members[msg.sender].role != RoleList.president) throw;
+    }
 
-    function Dao1901(address _secretaire, address _president, address _tresorier){
-        setBureau(_secretaire,_president,_tresorier);
+
+    function Dao1901(){
+      members[msg.sender].role = RoleList.founder;
+    }
+
+    // TO DO : Implement a real democracy
+    function setBureau(address _secretary, address _president, address _treasurer) isRole(RoleList.founder) {
+
+        if(created == true) throw;
+
+        members[_president].role = RoleList.president;
+        members[_secretary].role = RoleList.secretary;
+        members[_treasurer].role = RoleList.treasurer;
+
+        created = true;
+
     }
     
-    // TO DO : Implement a real democracy
-    function setBureau(address _secretaire, address _president, address _tresorier) isRole(RoleList.president) {
-        members[_president].role = RoleList.president;
-        members[_secretaire].role = RoleList.secretary;
-        members[_tresorier].role = RoleList.treasurer;
-    }
+    // function createMember(address _address, bool payed, bool vote) isAdmin() {
+    //     members[_address].canVote = vote;
+    //     members[_address].role = RoleList.member;
+    //     members[_address].key = _address;
+    //     members[_address].payed = payed;
+    // }
+    
+    // function createBooleanVote(string questions) isRole(RoleList.president){
+    //     votes[id].questions = questions;
+    //     votes[id].end = "need to find a systeme"
+    // }
 }
