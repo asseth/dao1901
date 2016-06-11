4265e4799da124876614a2d3c86ebdd161727119
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-06-11 16:48:03 +0200

    debug collaborating

---
+++ truffle/app/javascripts/app.js
@@ -0,0 +1,7 @@
+function test(message) {
+  var president = document.getElementById("president");
+  var tresorier = document.getElementById("tresorier");
+  var secretaire = document.getElementById("secretaire");
+  console.log(president, tresorier, secretaire);
+  status.innerHTML = message;
+};
