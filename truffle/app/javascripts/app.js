73bb503ec33b780db613875548d2e1af0922cca9
Altais <cervantescedric@gmail.com>
2016-06-11 22:40:15 +0200

    merge

--- truffle/app/javascripts/app.js
+++ truffle/app/javascripts/app.js
@@ -1,19 +1,6 @@
 var accounts;
 var account;
 
-function test(message) {
-  var president = document.getElementById("president").value.toString();
-  var tresorier = document.getElementById("tresorier").value.toString();
-  var secretaire = document.getElementById("secretaire").value.toString();
-  var dao = Dao1901.deployed();
-  console.log(dao)
-  console.log(president, tresorier, secretaire)
-  dao.setBureau.sendTransaction(secretaire, president, tresorier, {from: account}).then(function(message) {
-  	console.log(message)
-  }).catch(function(e) {
-    console.log(e);
-  });
-};
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
     if (err != null) {
@@ -33,10 +20,40 @@ window.onload = function() {
 }
 $(function () {
   $('#deploy').click(function() {
-    var president = document.getElementById("president").value;
-    var tresorier = document.getElementById("tresorier").value;
-    var secretaire = document.getElementById("secretaire").value;
-    console.log(president, tresorier, secretaire);
+    // var president = document.getElementById("president").value;
+    // var tresorier = document.getElementById("tresorier").value;
+    // var secretaire = document.getElementById("secretaire").value;
+
+    var president = "0x5497efa684f5de74ea2e326274d3484a039bb1db";
+    var tresorier = "0xbb2d2de3846b179359e8703a89578954b7f66769";
+    var secretaire = "0x544a56943ff9685fd3a745aa0f92db57ce2d0643";
+    var dao = Dao1901.deployed();
+    console.log(dao)
+    //   dao.createMember.sendTransaction(president, {from: account}).then(function(message) {
+    //  console.log(message)
+    // }).catch(function(e) {
+    //   console.log(e);
+    // });
+    dao.setBureau.sendTransaction(secretaire, president, tresorier, {from: account}).then(function(message) {
+      console.log(message)
+    }).catch(function(e) {
+      console.log(e);
+    });
+    dao.getMemberSince.call(1, {from: account}).then(function(message) {
+      console.log(message)
+    }).catch(function(e) {
+      console.log(e);
+    });
+    dao.getMemberCanVote.call(1, {from: account}).then(function(message) {
+      console.log(message)
+    }).catch(function(e) {
+      console.log(e);
+    });
+    dao.getMemberLength.call({from: account}).then(function(message) {
+      console.log(message)
+    }).catch(function(e) {
+      console.log(e);
+    });
     // TODO : déployer le contrat avec les membres du bureau
     // TODO : ouvrir page
   });
