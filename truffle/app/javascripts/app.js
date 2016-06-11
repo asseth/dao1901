e72eada63beb75476dbf875a0e5a387ed48f17c6
Altais <cervantescedric@gmail.com>
2016-06-11 20:19:54 +0200

    modification dao

--- truffle/app/javascripts/app.js
+++ truffle/app/javascripts/app.js
@@ -1,7 +1,39 @@
+var accounts;
+var account;
 function test(message) {
-  var president = document.getElementById("president").value;
-  var tresorier = document.getElementById("tresorier").value;
-  var secretaire = document.getElementById("secretaire").value;
-  console.log(president, tresorier, secretaire);
-  status.innerHTML = message;
+  var president = document.getElementById("president").value.toString();
+  var tresorier = document.getElementById("tresorier").value.toString();
+  var secretaire = document.getElementById("secretaire").value.toString();
+  var dao = Dao1901.deployed();
+  console.log(dao)
+  console.log(president, tresorier, secretaire)
+  dao.setBureau.sendTransaction(secretaire, president, tresorier, {from: account}).then(function(message) {
+  	console.log(message)
+  }).catch(function(e) {
+    console.log(e);
+  });
+
+
+  // dao.members.call().then(function(message) {
+  // 	console.log(message)
+  // }).catch(function(e) {
+  //   console.log(e);
+  // });
 };
+window.onload = function() {
+  web3.eth.getAccounts(function(err, accs) {
+    if (err != null) {
+      alert("There was an error fetching your accounts.");
+      return;
+    }
+
+    if (accs.length == 0) {
+      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+      return;
+    }
+
+    accounts = accs;
+    account = accounts[0];
+
+  });
+}
--- truffle/contracts/Dao1901.sol
+++ truffle/contracts/Dao1901.sol
@@ -3,13 +3,14 @@ contract Dao1901 {
     enum RoleList { founder, secretary, president, treasurer, member } // Role Available in the DAO
 
     struct Member {
-        address key;
+        uint memberSince;
         bool payed;
         bool canVote;
         RoleList role;
     }
-
-    mapping(address => Member) members;
+    
+    Member[] public members
+    mapping(address => uint) public memberId;
 
     bool created;
 
@@ -24,29 +25,35 @@ contract Dao1901 {
     }
 
 
-    function Dao1901(){
-      members[msg.sender].role = RoleList.founder;
+    function Dao1901(address _secretary, address _president, address _treasurer){
+        createMember(_secretary, true, true);
+        memberId[_secretary]
+        createMember(_president, true, true);
+        createMember(_treasurer, true, true);
     }
 
     // TO DO : Implement a real democracy
     function setBureau(address _secretary, address _president, address _treasurer) isRole(RoleList.founder) {
 
         if(created == true) throw;
-
-        members[_president].role = RoleList.president;
-        members[_secretary].role = RoleList.secretary;
-        members[_treasurer].role = RoleList.treasurer;
+        
 
         created = true;
-
+    }
+    function getMembers() {
+        
     }
     
-    // function createMember(address _address, bool payed, bool vote) isAdmin() {
-    //     members[_address].canVote = vote;
-    //     members[_address].role = RoleList.member;
-    //     members[_address].key = _address;
-    //     members[_address].payed = payed;
-    // }
+    function createMember(address _address, bool _payed, bool _vote) isAdmin() {
+        memberId[_address] = members.length;
+        id = members.length++;
+        members[id] = Member({
+            canVote: _vote, 
+            role: RoleList.member, 
+            memberSince: now, 
+            payed: payed
+        });
+    }
     
     // function createBooleanVote(string questions) isRole(RoleList.president){
     //     votes[id].questions = questions;
