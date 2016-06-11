741d3401c5bfb954480b39720f30d315f91f7857
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-06-11 16:36:17 +0200

    add 3 inputs

--- truffle/environments/development/build/app.js
+++ truffle/environments/development/build/app.js
@@ -5735,73 +5735,6 @@ var Pudding =
 (function() {
 
   var contract_data = {
-    abi: [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"conversionRate","type":"uint256"}],"name":"convert","outputs":[{"name":"convertedAmount","type":"uint256"}],"type":"function"}],
-    binary: "606060405260358060106000396000f36503050000000050606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
-    unlinked_binary: "606060405260358060106000396000f36503050000000050606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
-    address: "0xc305c901078781c232a2a521c2af7980f8385ee9",
-    generated_with: "2.0.9",
-    contract_name: "ConvertLib"
-  };
-
-  function Contract() {
-    if (Contract.Pudding == null) {
-      throw new Error("ConvertLib error: Please call load() first before creating new instance of this contract.");
-    }
-
-    Contract.Pudding.apply(this, arguments);
-  };
-
-  Contract.load = function(Pudding) {
-    Contract.Pudding = Pudding;
-
-    Pudding.whisk(contract_data, Contract);
-
-    // Return itself for backwards compatibility.
-    return Contract;
-  }
-
-  Contract.new = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("ConvertLib error: Please call load() first before calling new().");
-    }
-
-    return Contract.Pudding.new.apply(Contract, arguments);
-  };
-
-  Contract.at = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("ConvertLib error: Please call load() first before calling at().");
-    }
-
-    return Contract.Pudding.at.apply(Contract, arguments);
-  };
-
-  Contract.deployed = function() {
-    if (Contract.Pudding == null) {
-      throw new Error("ConvertLib error: Please call load() first before calling deployed().");
-    }
-
-    return Contract.Pudding.deployed.apply(Contract, arguments);
-  };
-
-  if (typeof module != "undefined" && typeof module.exports != "undefined") {
-    module.exports = Contract;
-  } else {
-    // There will only be one version of Pudding in the browser,
-    // and we can use that.
-    window.ConvertLib = Contract;
-  }
-
-})();
-;
-
-// Factory "morphs" into a Pudding class.
-// The reasoning is that calling load in each context
-// is cumbersome.
-
-(function() {
-
-  var contract_data = {
     abi: [{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalanceInEth","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[],"type":"constructor"}],
     binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101548061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b1600435600073c305c901078781c232a2a521c2af7980f8385ee96396e4ee3d6100d784610092565b6100c360043560243533600160a060020a03166000908152602081905260408120548290101561011c5761014e565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b604080519115158252519081900360200190f35b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac9050565b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b9291505056",
     unlinked_binary: "6060604052600160a060020a033216600090815260208190526040902061271090556101548061002f6000396000f3606060405260e060020a60003504637bd703e8811461003157806390b98a111461005c578063f8b2cb4f1461008b575b005b6100b1600435600073__ConvertLib____________________________6396e4ee3d6100d784610092565b6100c360043560243533600160a060020a03166000908152602081905260408120548290101561011c5761014e565b6100b16004355b600160a060020a0381166000908152602081905260409020545b919050565b60408051918252519081900360200190f35b604080519115158252519081900360200190f35b60026040518360e060020a02815260040180838152602001828152602001925050506020604051808303818660325a03f4156100025750506040515191506100ac9050565b5033600160a060020a039081166000908152602081905260408082208054859003905591841681522080548201905560015b9291505056",
@@ -5862,6 +5795,73 @@ var Pudding =
 })();
 ;
 
+// Factory "morphs" into a Pudding class.
+// The reasoning is that calling load in each context
+// is cumbersome.
+
+(function() {
+
+  var contract_data = {
+    abi: [{"constant":false,"inputs":[{"name":"amount","type":"uint256"},{"name":"conversionRate","type":"uint256"}],"name":"convert","outputs":[{"name":"convertedAmount","type":"uint256"}],"type":"function"}],
+    binary: "606060405260358060106000396000f36503050000000050606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
+    unlinked_binary: "606060405260358060106000396000f36503050000000050606060405260e060020a600035046396e4ee3d81146024575b6007565b602435600435026060908152602090f3",
+    address: "0xc305c901078781c232a2a521c2af7980f8385ee9",
+    generated_with: "2.0.9",
+    contract_name: "ConvertLib"
+  };
+
+  function Contract() {
+    if (Contract.Pudding == null) {
+      throw new Error("ConvertLib error: Please call load() first before creating new instance of this contract.");
+    }
+
+    Contract.Pudding.apply(this, arguments);
+  };
+
+  Contract.load = function(Pudding) {
+    Contract.Pudding = Pudding;
+
+    Pudding.whisk(contract_data, Contract);
+
+    // Return itself for backwards compatibility.
+    return Contract;
+  }
+
+  Contract.new = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("ConvertLib error: Please call load() first before calling new().");
+    }
+
+    return Contract.Pudding.new.apply(Contract, arguments);
+  };
+
+  Contract.at = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("ConvertLib error: Please call load() first before calling at().");
+    }
+
+    return Contract.Pudding.at.apply(Contract, arguments);
+  };
+
+  Contract.deployed = function() {
+    if (Contract.Pudding == null) {
+      throw new Error("ConvertLib error: Please call load() first before calling deployed().");
+    }
+
+    return Contract.Pudding.deployed.apply(Contract, arguments);
+  };
+
+  if (typeof module != "undefined" && typeof module.exports != "undefined") {
+    module.exports = Contract;
+  } else {
+    // There will only be one version of Pudding in the browser,
+    // and we can use that.
+    window.ConvertLib = Contract;
+  }
+
+})();
+;
+
 
 
 
@@ -5890,5 +5890,5 @@ if (typeof web3 !== 'undefined') {
 
 Pudding.setWeb3(window.web3);                                 
 
-Pudding.load([ConvertLib, MetaCoin], window);               
+Pudding.load([MetaCoin, ConvertLib], window);               
 
