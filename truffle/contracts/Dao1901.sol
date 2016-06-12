b850fae1c38996e50cbcc981226e1a018d4c0d83
Fabien <fabien@acathla.tk>
2016-06-12 11:07:21 +0200

    Ajout de commentaires sur .sol

--- truffle/contracts/Dao1901.sol
+++ truffle/contracts/Dao1901.sol
@@ -3,12 +3,13 @@ contract Dao1901 {
     enum RoleList { founder, secretary, president, treasurer, member } // Role Available in the DAO
 
     struct Member {
-        uint memberSince;
-        bool payed;
-        bool canVote;
-        RoleList role;
-        address public_key;
+        uint memberSince;	// Registering date of a member
+        bool payed;		// True if member has payed, if needed
+        bool canVote;		// True if member is allowed to vote
+        RoleList role;		
+        address public_key;	// address of the member
     }
+    // Functions to query the database of members
     function getMemberSince(uint id) returns(uint memberSince){
         return members[id].memberSince;
     }
@@ -29,30 +30,30 @@ contract Dao1901 {
     }
 
     
-    Member[] public members;
-    mapping (address => uint) public memberId;
+    Member[] public members;	// Dynamic table of members (in storage)
+    mapping (address => uint) public memberId;	// A mapping to find a member by it's address
 
-    bool created;
+    bool created;	// A dirty flag
 
     modifier isRole(RoleList role) {
         if (memberId[msg.sender] == 0 || members[memberId[msg.sender]].role != role) return;
         _
     }
 
-    modifier isAdmin() {
+    modifier isAdmin() {	// Checks if sender is not a normal member and exists in database == is an admin
         if (memberId[msg.sender] == 0 || members[memberId[msg.sender]].role == RoleList.member) return;
         _
     }
 
 
     function Dao1901(){
-        members.length += 2;
-        members[0] = Member({canVote: false, role: RoleList.member, memberSince: now, payed: false, public_key: 0}); // utilisateur bidon
-        members[1] = Member({canVote: false, role: RoleList.founder, memberSince: now, payed: false, public_key: msg.sender});
-        memberId[msg.sender] = 1;
+        members.length += 2;	// Adding two members to the table
+        members[0] = Member({canVote: false, role: RoleList.member, memberSince: now, payed: false, public_key: 0}); // One fake user for the checks on the mapping to work
+        members[1] = Member({canVote: false, role: RoleList.founder, memberSince: now, payed: false, public_key: msg.sender});	// The founder
+        memberId[msg.sender] = 1;	// Only a real user needs an updated mapping
     }
 
-    // TO DO : Implement a real democracy
+    // Called only once by the founder
     function setBureau(address _secretary, address _president, address _treasurer) isRole(RoleList.founder) {
 
         if(created == true) return;
@@ -69,18 +70,16 @@ contract Dao1901 {
     
     function createMember(address _address, bool _payed, bool _vote) isAdmin() {
         uint id;
-        memberId[_address] = members.length;
-        id = members.length++;
+        memberId[_address] = id = members.length;	// Update of the mapping : new member's address to the last index of the table
+        members.length++;
         members[id] = Member({
             canVote: _vote, 
             role: RoleList.member, 
             memberSince: now, 
             payed: _payed,
-            public_key: msg.sender
+            public_key: _address
         });
     }
-    
-
     
     // function createBooleanVote(string questions) isRole(RoleList.president){
     //     votes[id].questions = questions;
