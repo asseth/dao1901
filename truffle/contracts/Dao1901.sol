eb571b9298a4c584455d4969f40c038044629dc6
Alexandre Kurth <alexandre.kurth@gmail.com>
2016-06-12 15:45:22 +0200

    add validation for proposal

--- truffle/contracts/Dao1901.sol
+++ truffle/contracts/Dao1901.sol
@@ -1,20 +1,22 @@
-contract Dao1901 {
+*contract Dao1901 {
 
     enum RoleList { founder, secretary, president, treasurer, member } // Role Available in the DAO
 
+    enum VoteState { proposed, accepted, voted, rejected }  // Accepted by "bureau", voted by members
+
     struct Member {
-        uint memberSince;	// Registering date of a member
-        bool payed;		// True if member has payed, if needed
-        bool canVote;		// True if member is allowed to vote
-        RoleList role;		
-        address public_key;	// address of the member
+        uint memberSince;   // Registering date of a member
+        bool payed;     // True if member has payed, if needed
+        bool canVote;       // True if member is allowed to vote
+        RoleList role;      
+        address public_key; // address of the member
     }
 
 
     
-    Member[] public members;	// Dynamic table of members (in storage)
-    mapping (address => uint) public memberId;	// A mapping to find a member by it's address
-    bool created;	// A dirty flag
+    Member[] public members;    // Dynamic table of members (in storage)
+    mapping (address => uint) public memberId;  // A mapping to find a member by it's address
+    bool created;   // A dirty flag
 
 
     // Functions to query the database of members
@@ -42,7 +44,7 @@ contract Dao1901 {
         _
     }
 
-    modifier isAdmin() {	// Checks if sender is not a normal member and exists in database == is an admin
+    modifier isAdmin() {    // Checks if sender is not a normal member and exists in database == is an admin
         if (memberId[msg.sender] == 0 || members[memberId[msg.sender]].role == RoleList.member) throw;
         _
     }
@@ -54,10 +56,10 @@ contract Dao1901 {
 
 
     function Dao1901(){
-        members.length += 2;	// Adding two members to the table
+        members.length += 2;    // Adding two members to the table
         members[0] = Member({canVote: false, role: RoleList.member, memberSince: now, payed: false, public_key: 0}); // One fake user for the checks on the mapping to work
-        members[1] = Member({canVote: false, role: RoleList.founder, memberSince: now, payed: false, public_key: msg.sender});	// The founder
-        memberId[msg.sender] = 1;	// Only a real user needs an updated mapping
+        members[1] = Member({canVote: false, role: RoleList.founder, memberSince: now, payed: false, public_key: msg.sender});  // The founder
+        memberId[msg.sender] = 1;   // Only a real user needs an updated mapping
     }
 
     // Called only once by the founder
@@ -79,7 +81,7 @@ contract Dao1901 {
     // TODO : fire the founder of the admins
     function createMember(address _address, bool _payed, bool _vote) isAdmin() {
         uint id;
-        memberId[_address] = id = members.length;	// Update of the mapping : new member's address to the last index of the table
+        memberId[_address] = id = members.length;   // Update of the mapping : new member's address to the last index of the table
         members.length++;
         members[id] = Member({
             canVote: _vote, 
@@ -95,12 +97,11 @@ contract Dao1901 {
     //     votes[id].end = "need to find a systeme"
     // }
 
-    votePreProposals[] public proposal; // have to be validated by "bureau"
-    voteProposal[] public proposal; // once validated by bureau, have to be voted by members
-    rejectedProposals[] public proposal; 
+    BooleanProposal[] public voteProposals; // once validated by bureau, have to be voted by members
+    //BooleanProposal[] public rejectedProposals; 
 
 
-     struct BooleanProposal {
+    struct BooleanProposal {
         string proposalContent;
         uint openSince;
         uint yes;
@@ -108,26 +109,37 @@ contract Dao1901 {
         address hasVoted[];
         mapping(address => uint) whoHasVoted; 
         bool validatedByBureau=false;
+        VoteState state;
     }
 
 
-    function votePreProposal(uint _id) isAdmin(){
+    function votePreProposal(uint _id), bool vote) isAdmin(){
         uint id; // id for hasVoted[]
 
-        // checks if has voted or not
-        if( votePreProposals[_id].whoHasVoted[msg.sender] == 0){ // if returns 0, never voted
+        // if person has has ever voted, can't vote
+        // prevent multiple votes
+        if( voteProposals[_id].whoHasVoted[msg.sender] != 0){ // if returns 0, never voted
+            throw;
+        }
+        // Vote
+        id=voteProposals[_id].hasVoted.length;
+        voteProposals[_id].hasVoted.length++;
+        voteProposals[_id].hasVoted[id]=msg.sender;
+        if vote voteProposals[_id].yes++ else voteProposals[_id].no++ ;            
+        
+        // verify the end of the vote 
+        if (voteProposals[_id].yes + voteProposals[_id].no) == 3 {
             
+            if( voteProposals[_id].yes >= voteProposals[_id].no ){
+                voteProposals[_id].state=validated;
+            } else {
+                
+            }
+            // Reset the votes (because state is chosen as accepted or rejected, then members can vote)
+            voteProposals[_id].yes=0;
+            voteProposals[_id].no=0;
         }
 
-
-
-        if (votePreProposals[_id].yes + votePreProposals[_id].no) >= 2 {
-            id=votePreProposals[_id].hasVoted.length;
-            votePreProposals[_id].hasVoted.length++;
-            votePreProposals[_id].hasVoted[id]=msg.sender;
-            
-        }
-        // TODO : check
 
     }
     function sendProposal(string _proposalContent) hasRightToProposeOrVote(){
@@ -136,6 +148,9 @@ contract Dao1901 {
         voteProposals.length++;
         voteProposals[id].string=_proposalContent;
         voteProposals[id].openSince=now;
+        voteProposals[id].state=proposed;
+        voteProposals[id].hasVoted.length++; // hasVoted[0] will never contain an address (so the tests on mapping will work)
+
     }
 
 }
