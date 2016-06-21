2d45512734987cae71b6332064080e8928b3916e
Mathieu Sornay <mathieu.sornay@ercom.fr>
2016-06-21 17:54:18 +0200

    Comments on DAO1901Member

--- truffle/contracts/Dao1901Members.sol
+++ truffle/contracts/Dao1901Members.sol
@@ -1,22 +1,45 @@
 contract Dao1901Members {
 
+  /* The owner of the contract can add members and transfer membership.
+     initial value is the contract creator */
   address public owner = msg.sender;
 
-  address public head;
+  address public head;          /* linked list */
 
   /* subscription entry */
   struct sub {
     uint end;                   /* time at which subscription ends */
-    address next;               /* linked list */
+    address next;
   }
 
+  /* subscription storage.
+
+     The list of the DAO1901 members can be constructed like this :
+
+     addr = head
+     members = []
+     while True:
+         if isMember(addr):
+             members.append(addr)
+         addr = subscriptions[addr].next
+         if addr == 0x00:
+           break
+
+   */
   mapping (address => sub) public subscriptions;
+
+  /* Check that a given address is subscribed to the DAO1901 */
+  function isMember(address _member) returns (bool) {
+    return subscriptions[_member].end >= now;
+  }
 
   modifier ownerOnly() {
     if (msg.sender != owner) throw;
     _
   }
 
+  /* add / update (i.e. renew )the subscription of a member for a given
+     number of years */
   function subscribe(address _member, uint _yearsDuration) ownerOnly {
     if (subscriptions[_member].end == 0) { /* first subscription */
       subscriptions[_member].next = head;
@@ -25,12 +48,9 @@ contract Dao1901Members {
     subscriptions[_member].end = now + _yearsDuration * 1 years;
   }
 
+  /* transfer DAO1901 ownership */
   function changeOwner(address _newOwner) ownerOnly {
     owner = _newOwner;
-  }
-
-  function canVote(address _member) returns (bool) {
-    return subscriptions[_member].end >= now;
   }
 
 }
