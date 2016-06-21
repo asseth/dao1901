4bb44aa7e19ba2b4e7ca38f321691e7efa5a9bb7
Mathieu Sornay <mathieu.sornay@ercom.fr>
2016-06-21 17:38:57 +0200

    simplify code & changeOwner function

--- truffle/contracts/Dao1901Members.sol
+++ truffle/contracts/Dao1901Members.sol
@@ -1,39 +1,36 @@
 contract Dao1901Members {
 
-  address public owner;
+  address public owner = msg.sender;
+
   address public head;
 
-  struct node {
-    uint end;
-    address next;
+  /* subscription entry */
+  struct sub {
+    uint end;                   /* time at which subscription ends */
+    address next;               /* linked list */
   }
 
-  mapping (address => node) public subscriptions;
-
-  function Dao1901Members() {
-    head = msg.sender;          /* First member is the creator */
-    owner = msg.sender;
-
-    subscriptions[owner].end = now + 3 years;
-  }
+  mapping (address => sub) public subscriptions;
 
   modifier ownerOnly() {
     if (msg.sender != owner) throw;
     _
   }
 
-  function subscribe(address _member, uint duration) ownerOnly {
-    if (subscriptions[_member] == 0) {
-      subscriptions[_head].next = _member;
+  function subscribe(address _member, uint _yearsDuration) ownerOnly {
+    if (subscriptions[_member].end == 0) { /* first subscription */
+      subscriptions[_member].next = head;
       head = _member;
     }
-    subscriptions[_member].end = now + duration;
+    subscriptions[_member].end = now + _yearsDuration * 1 years;
   }
 
-  function canVote(address _member) returns bool {
-    var sub = subscriptions[_member];
-    if (sub == 0 || sub.end == 0) return false;
-    return sub.end >= now;
+  function changeOwner(address _newOwner) ownerOnly {
+    owner = _newOwner;
+  }
+
+  function canVote(address _member) returns (bool) {
+    return subscriptions[_member].end >= now;
   }
 
 }
