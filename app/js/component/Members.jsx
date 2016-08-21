ce5f80400b042b21e917c29d8f0601b838c39530
Stephane Roche <stephane.roche@anaplan.com>
2016-08-21 20:20:16 +0200

    Add member

--- app/js/component/Members.jsx
+++ app/js/component/Members.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { FormControl, FormGroup } from 'react-bootstrap';
+import { Button, Form, FormControl, FormGroup } from 'react-bootstrap';
 import { Dao1901Members, web3 } from '../../../contracts/Dao1901Members.sol';
 
 export default class Votes extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      memberAddress: '',
+      validationMemberAddress: null,
+      validationYearsDuration: null,
+      yearsDuration: ''
+    };
+
+    this.handleMemberAddressChange = this.handleMemberAddressChange.bind(this);
+    this.handleYearsDurationChange = this.handleYearsDurationChange.bind(this);
     this.memberList = this.memberList.bind(this);
+    this.subscribe = this.subscribe.bind(this);
   }
+
 
   memberList() {
     var members = [];
@@ -21,24 +32,78 @@ export default class Votes extends React.Component {
     return members;
   }
 
+  handleMemberAddressChange(e) {
+    this.setState({[e.target.name]: e.target.value}, () => {
+      // Validation
+      if (this.state.memberAddress) {
+        this.setState({validationMemberAddress: web3.isAddress(this.state.memberAddress) ? 'success' : 'error'})
+      } else {
+        this.setState({validationMemberAddress: null})
+      }
+    });
+  }
+
+  handleYearsDurationChange(e) {
+    this.setState({[e.target.name]: e.target.value}, () => {
+      // Validation
+      if (this.state.yearsDuration) {
+        this.setState({validationYearsDuration: Number.isInteger(Number(this.state.yearsDuration)) ? 'success' : 'error'})
+      } else {
+        this.setState({validationYearsDuration: null})
+      }
+    });
+  }
+
+  subscribe(e) {
+    e.preventDefault();
+    console.log('this.state.memberAddress, this.state.yearsDuration', this.state.memberAddress, this.state.yearsDuration);
+    Dao1901Members.subscribe(this.state.memberAddress, this.state.yearsDuration);
+  }
+
+
   render() {
     console.log('memberList', this.memberList());
     return (
       <div className="Dao1901Members">
         <h2>Dao1901Members</h2>
-
-        <form>
+        <h3>Add a member</h3>
+        <Form>
           <FormGroup
-            controlId="formBasicText"
+            controlId="memberAddress"
+            validationState={this.state.validationMemberAddress}
           >
             <FormControl
+              name="memberAddress"
+              onChange={this.handleMemberAddressChange}
+              placeholder="Enter the member Ethereum address"
               type="text"
-              value={''}
-              placeholder="Enter text"
-              onChange={() => ''}
+              value={this.state.memberAddress}
             />
+            <FormControl.Feedback />
           </FormGroup>
-        </form>
+
+          <FormGroup
+            controlId="yearsDuration"
+            validationState={this.state.validationYearsDuration}
+          >
+            <FormControl
+              name="yearsDuration"
+              onChange={this.handleYearsDurationChange}
+              placeholder="Enter the number of years the subscription will last"
+              type="text"
+              value={this.state.yearsDuration}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
+          <Button
+            bsStyle="primary"
+            onClick={this.subscribe}
+            type="submit"
+          >
+            Add Member
+          </Button>
+        </Form>
+
 
         <dl>
           <dt>Address</dt>
