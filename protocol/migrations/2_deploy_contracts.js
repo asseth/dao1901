a3293836dea79adb403e0c6e11a8da2a472d7668
Janaka-Steph <rochestephane@yahoo.fr>
2017-05-20 20:37:53 +0200

    Add logs to deploy script

--- protocol/migrations/2_deploy_contracts.js
+++ protocol/migrations/2_deploy_contracts.js
@@ -3,7 +3,10 @@ let Dao1901Votes = artifacts.require("Dao1901Votes");
 let Owned = artifacts.require("Owned");
 
 module.exports = (deployer) => {
-  deployer.deploy(Owned);
-  deployer.deploy(Dao1901Members)
-    .then(() => deployer.deploy(Dao1901Votes, Dao1901Members.address));
+  deployer.deploy(Owned)
+    .then(() => console.log(`Owned has been deployed at: ${Owned.address}`))
+    .then(() => deployer.deploy(Dao1901Members))
+    .then(() => console.log(`Dao1901Members has been deployed at: ${Dao1901Members.address}`))
+    .then(() => deployer.deploy(Dao1901Votes, Dao1901Members.address))
+    .then(() => console.log(`Dao1901Votes has been deployed at: ${Dao1901Votes.address}`))
 };
