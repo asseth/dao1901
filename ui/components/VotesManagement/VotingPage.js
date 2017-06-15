c66a7f1099a0f24f38ac51046c5b9df6d7f3cbed
Janaka-Steph <rochestephane@yahoo.fr>
2017-06-15 20:25:39 +0200

    styling

--- ui/components/VotesManagement/VotingPage.js
+++ ui/components/VotesManagement/VotingPage.js
@@ -2,30 +2,40 @@ import React from 'react'
 import MultipleLists from '../common/MultipleLists'
 import VoteForm from './VotingForm'
 import VotesListItem from './VotesListItem'
-
-function VotingPage (props) {
+function VotingPage(props) {
   const {onVoteSubmit, votes} = props
-
   return (
     <div id="VotingPageContainer">
-      <VoteForm
-        onVoteSubmit={onVoteSubmit}
-      />
+      <div className="row">
+        <div className="col-12">
+          <h2>Vote for a Proposal</h2>
+          <VoteForm
+            onVoteSubmit={onVoteSubmit}
+          />
+        </div>
+      </div>
 
-      <h2>What voters said for each proposal</h2>
-      <MultipleLists
-        component={VotesListItem}
-        items={votes}
-      />
+      <div className="m-top-50 row">
+        <div className="col-12">
+          <h2>What voters said for each proposal</h2>
+          <MultipleLists
+            component={VotesListItem}
+            items={votes}
+          />
+        </div>
+      </div>
 
-      <h2>Running Proposals</h2>
-      <p>Proposal _name_ _number_: will be settle on the 25 of June 2017</p>
-      <p>Proposal _name_ _number_: will be settle on the 04 of August 2017</p>
-      <p>Proposal _name_ _number_: will be settle on the 18 of October 2017</p>
-      <p>Proposal _name_ _number_: will be settle on the 26 of October 2017</p>
+      <div className="m-top-50 row">
+        <div className="col-12">
+          <h2>Running Proposals</h2>
+          <p>Proposal _name_ _number_: will be settle on the 25 of June 2017</p>
+          <p>Proposal _name_ _number_: will be settle on the 04 of August 2017</p>
+          <p>Proposal _name_ _number_: will be settle on the 18 of October 2017</p>
+          <p>Proposal _name_ _number_: will be settle on the 26 of October 2017</p>
+        </div>
+      </div>
     </div>
   )
 }
-
 export default VotingPage
 
