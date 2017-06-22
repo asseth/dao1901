9defdcd3a9b4acf4621cc93efba58d80d0ea759f
Janaka-Steph <rochestephane@yahoo.fr>
2017-06-22 15:00:33 +0200

    Fix jest es6 compiling

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   roots: ['ui'],
+  transformIgnorePatterns: [
+    "node_modules/(?!dao1901-contracts)"
+  ]
 };
--- ui/index.js
+++ ui/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import RootContainer from './containers/RootContainer'
-import createStore, {sagaMiddleware} from './redux/createStore';
+import createStore, {sagaMiddleware} from './redux/createStore'
 // Global styles
-import './assets/theme/app.scss';
+import './assets/theme/app.scss'
 
 // ========================================================
 // Store Instantiation
@@ -36,15 +36,15 @@ if (module.hot) {
   });
 
   module.hot.accept('./redux/reducersIndex', () => {
-    const reducers = require('./redux/reducersIndex').default;
+    const reducers = require('./redux/reducersIndex').default
     return store.replaceReducer(reducers);
   });
 
   module.hot.accept('./redux/sagasIndex', () => {
-    const newSagas = require('./redux/sagasIndex').default;
+    const newSagas = require('./redux/sagasIndex').default
     sagaTask.cancel()
     sagaTask.done.then(() => {
-      sagaTask = sagaMiddleware.run(newSagas);
+      sagaTask = sagaMiddleware.run(newSagas)
     })
   })
 }
