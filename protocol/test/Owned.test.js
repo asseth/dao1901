b142347b078c889320525ad86d0bad5e084768c2
Janaka-Steph <rochestephane@yahoo.fr>
2017-06-27 19:53:10 +0200

    Fix truffle tests

---
+++ protocol/test/Owned.test.js
@@ -0,0 +1,33 @@
+const Owned = artifacts.require('Owned')
+let expectThrow = require('../helpers/expectThrow.js')
+let alice, bob, carol;
+// let itClean = (title, itCb) => contract(title, () => it(title, itCb))
+
+let owned = null;
+
+contract('Owned', () => {
+  before(async () => {
+    owned = await Owned.deployed()
+    alice = web3.eth.accounts[0];
+    console.log('alice: ', alice);
+    bob = web3.eth.accounts[1];
+    console.log('bob: ', bob);
+    carol = web3.eth.accounts[2];
+    console.log('carol: ', carol);
+  })
+
+  it('should have an address property', () => {
+    assert(owned.address, "contract is not deployed")
+  });
+
+  it('should not be able to transfer DAO1901 ownership', async () => {
+    await expectThrow(owned.changeOwner.sendTransaction(carol, {from: carol}))
+  })
+
+  it('should transfer DAO1901 ownership', async () => {
+    const tx = await owned.changeOwner.sendTransaction(bob, {from: alice, gas: 200000})
+    assert(tx, 'tx')
+    assert.equal(await owned.owner(), bob, "ownership was not transfered to Bob")
+    await expectThrow(owned.changeOwner.sendTransaction(bob, {from: alice}))
+  })
+})
--- protocol/truffle.js
+++ protocol/truffle.js
@@ -1,12 +1,23 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register');
+//require('babel-register')
+require("babel-polyfill")
+
 
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*',        // Match any network id
+      before_timeout: 200000, // specify Mocha timeout per network
+      test_timeout: 300000    // specify Mocha timeout per network
     }
+  },
+  deploy: [
+    "Owned",
+  ],
+  mocha: {
+    timeout: 200000,
+    useColors: true
   }
-};
+}
