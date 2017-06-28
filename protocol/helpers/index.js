df921018ab2ad209c06d4d20f79e887d02d46605
Janaka-Steph <rochestephane@yahoo.fr>
2017-06-28 17:17:45 +0200

    Add new test helpers

---
+++ protocol/helpers/index.js
@@ -0,0 +1,51 @@
+export const expectThrow = async promise => {
+  try {
+    await promise
+  } catch (error) {
+    // TODO: Check jump destination to destinguish between a throw
+    //       and an actual invalid jump.
+    const invalidJump = error.message.search('invalid JUMP') >= 0
+
+    // TODO: Found out in which circumstances 'invalid opcode' is throwed
+    const invalidOpcode = error.message.search('invalid opcode') >= 0
+
+    // TODO: When we contract A calls contract B, and B throws, instead
+    //       of an 'invalid jump', we get an 'out of gas' error. How do
+    //       we distinguish this from an actual out of gas event? (The
+    //       testrpc log actually show an 'invalid jump' event.)
+    const outOfGas = error.message.search('out of gas') >= 0
+    assert(invalidJump || invalidOpcode || outOfGas, "Expected throw, got '" + error + "' instead")
+    return
+  }
+  assert.fail('Expected throw not received')
+}
+
+export const mineBlocks = async function (num=1) {
+  for (let i=0; i<num; ++i) {
+    await new Promise(function(resolve, reject) { web3.currentProvider.sendAsync({ jsonrpc: "2.0", method: "evm_mine", id: i }, function(err, result) { resolve(); }); })
+  }
+}
+
+export const blockNumber = () => {
+  return new Promise(function(resolve, reject) {
+    web3.currentProvider.sendAsync({ jsonrpc: "2.0", method: "eth_blockNumber", id: 0x05 }, function(err, result) { resolve(parseInt(result.result)) })
+  })
+}
+
+export const snapshot = () => {
+  return new Promise(function(resolve, reject) {
+    web3.currentProvider.sendAsync({ jsonrpc: "2.0", method: "evm_snapshot", id: 0xabcd }, function(err, result) { resolve(); })
+  })
+}
+
+export const revert = () => {
+  return new Promise(function(resolve, reject) {
+    web3.currentProvider.sendAsync({ jsonrpc: "2.0", method: "evm_revert", id: 0xabcd }, function(err, result) { resolve(); })
+  })
+}
+
+export const reset = () => {
+  return new Promise(function(resolve, reject) {
+    web3.currentProvider.sendAsync({ jsonrpc: "2.0", method: "evm_reset", id: 0xabce }, function(err, result) { resolve(); })
+  })
+}
