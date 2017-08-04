7546d25336c08a2b9da21f1abf955dac0d11293f
Janaka-Steph <rochestephane@yahoo.fr>
2017-08-04 14:01:25 +0200

    refactoring

--- fuse.js
+++ fuse.js
@@ -1,8 +1,8 @@
-const {CSSModules, CSSPlugin, EnvPlugin, FuseBox, JSONPlugin, PostCSSPlugin, WebIndexPlugin, Sparky} = require("fuse-box")
+const {CSSModules, CSSPlugin, CSSResourcePlugin, EnvPlugin, FuseBox, JSONPlugin, QuantumPlugin, PostCSSPlugin, WebIndexPlugin, Sparky} = require("fuse-box")
 const express = require('express')
 const resolveId = require('postcss-import/lib/resolve-id')
 const path = require('path')
-const production = false
+const isProduction = process.env.NODE_ENV === 'production' ? true : false
 
 const POSTCSS_PLUGINS = [
   require("postcss-import")({
@@ -14,46 +14,57 @@ const POSTCSS_PLUGINS = [
   })
 ]
 
-const fuse = FuseBox.init({
-  cache: true,
-  experimentalFeatures: true,
-  target: 'browser',
-  homeDir: 'src',
-  modulesFolder: 'customModules',
-  output: 'build/$name.js',
-  log: true,
-  debug: true,
-  plugins: [
-    EnvPlugin({NODE_ENV: production ? "production" : "development"}),
-    [/components.*\.css$/, PostCSSPlugin(POSTCSS_PLUGINS), CSSModules(), CSSPlugin()],
-    [PostCSSPlugin(POSTCSS_PLUGINS), CSSPlugin()],
-    JSONPlugin(),
-    WebIndexPlugin({
-      template: "src/ui/index.html",
-      path: "/"
-    })
-  ]
-})
-const app = fuse.bundle("assets/app")
-  .instructions(`> ui/index.tsx`)
+Sparky.task('default', ['clean', 'copy-assets', 'build'], () => {})
+Sparky.task('clean', () => Sparky.src(path.resolve("build")).clean(path.resolve("build")))
+Sparky.task('clean-cache', () => Sparky.src(".fusebox/*").clean(".fusebox/"))
+Sparky.task('copy-assets', () => Sparky.src("assets/**/**.*", {base: "./src/ui"}).dest("build"))
+Sparky.task('build', () => {
+  const fuse = FuseBox.init({
+    cache: !isProduction,
+    experimentalFeatures: true, // remove next major release of fb
+    target: 'browser',
+    homeDir: 'src',
+    modulesFolder: 'customModules',
+    output: 'build/$name.js',
+    log: true,
+    debug: true,
+    sourceMaps: !isProduction,
+    useJsNext: true,
+    plugins: [
+      EnvPlugin({NODE_ENV: isProduction ? "production" : "development"}),
+      [/components.*\.css$/, PostCSSPlugin(POSTCSS_PLUGINS), CSSModules(), CSSPlugin()],
+      [PostCSSPlugin(POSTCSS_PLUGINS), CSSResourcePlugin({inline: true}), CSSPlugin()], // includes font-awesome in the bundle
+      JSONPlugin(),
+      WebIndexPlugin({
+        template: "src/ui/index.html",
+        path: "."
+      }),
+      isProduction && QuantumPlugin({
+        api: (core) => {
+          core.solveComputed("bn.js/lib/bn.js")
+        },
+        bakeApiIntoBundle: 'assets/app',
+        ensureES5 : true,
+        removeExportsInterop: true,
+        treeshake: false,
+        uglify: false
+      })
+    ]
+  })
 
-if (!production) { app.watch('**').sourceMaps(true) }
-
-Sparky.task('clean', () => Sparky.src(path.resolve("build")).clean(path.resolve("build")));
-Sparky.task('copy-assets', () => Sparky.src("assets/**/*.*", { base: "./src/ui"}).dest("build"));
-Sparky.task('run', ['clean', 'copy-assets'], () => {
-  fuse.dev({ open: false, port: 8085, root: 'build' }, server => {
-    const dist = path.resolve("src")
+  !isProduction && fuse.dev({ open: false, port: 8085, root: 'build' }, server => {
     const build = path.resolve("build")
-
     const app = server.httpServer.app
-    app.use("/assets/", express.static(path.join(build,'assets')));
+    app.use("/assets/", express.static(path.join(build,'assets')))
     app.get("*", function(req, res) {
-      res.sendFile(path.resolve("build", "index.html"));
+      res.sendFile(path.resolve("build", "index.html"))
     })
   })
 
-  fuse.run()
-});
+  const app = fuse.bundle("assets/app")
+    .instructions(`>ui/index.tsx`)
 
-Sparky.task('default', ['run'], () => {});
+  !isProduction && app.watch('**')
+
+  fuse.run()
+})
--- src/ui/components/VotesManagement/ProposalsListItem/index.tsx
+++ src/ui/components/VotesManagement/ProposalsListItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import * as PropTypes from 'prop-types'
 import styles from './styles.css'
 import convertTimestampToDate from '../../../helpers/convertTimestampToDate'
