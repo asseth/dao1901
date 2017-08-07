ede1d53e84759cf8037579abdccdb22892dc4fff
Janaka-Steph <rochestephane@yahoo.fr>
2017-08-07 18:06:26 +0200

    Add truffle-lightwallet-provider

--- customModules/protocol/truffle.js
+++ customModules/protocol/truffle.js
@@ -1,18 +1,49 @@
+require('dotenv').config({path: '../../.env'})
+const LightWalletProvider = require('@digix/truffle-lightwallet-provider')
+const { KEYSTORE, PASSWORD } = process.env
+if (!KEYSTORE || !PASSWORD) { throw new Error('You must export KEYSTORE and PASSWORD (see truffle.js)') }
+
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*',        // Match any network id
-      before_timeout: 200000, // specify Mocha timeout per network
-      test_timeout: 300000    // specify Mocha timeout per network
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'http://localhost:8545/',
+        debug: true, // optional, show JSON-RPC logs
+        prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this  of wei
+        pollingInterval: 500 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '*'
     },
     kovan: {
-      host: 'localhost',
-      port: 8545,
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://kovan.infura.io/',
+        pollingInterval: 2000, // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+        debug: false
+      }),
+      gas: 4600000,
       network_id: '42',
-      gas:4900000
-    }
+    },
+    ropsten: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://ropsten.infura.io/',
+        pollingInterval: 4000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
+      }),
+      network_id: '3',
+    },
+    mainnet: {
+      provider: new LightWalletProvider({
+        keystore: KEYSTORE,
+        password: PASSWORD,
+        rpcUrl: 'https://mainnet.infura.io/'
+      }),
+      network_id: '1',
+    },
   },
   mocha: {
     timeout: 200000,
--- src/ui/redux/createStore.ts
+++ src/ui/redux/createStore.ts
@@ -37,62 +37,62 @@ const middlewares = [reduxRouterMiddleware, sagaMiddleware, logger]
 // Get compose function from redux-devtools-extension if available
 // Todo Not used because of weird bug - it triggers eth_protocolVersion ethereum json-rpc on open
 const composeEnhancers =
-    typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-        }) : compose
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
 
 const finalCreateStore = composeEnhancers(
-    applyMiddleware(...middlewares),
-    //DevTools.instrument()
+  applyMiddleware(...middlewares),
+  //DevTools.instrument()
 )(createStore)
 // ======================================================
 // Store Instantiation
 // ======================================================
-const store = finalCreateStore(makeRootReducer());
+const store = finalCreateStore(makeRootReducer())
 
 // ======================================================
 // Set Web3
 // ======================================================
 window.addEventListener('load', function () {
-    // Set Web3
-    let web3Location = `http://${truffleConfig.networks.development.host}:${truffleConfig.networks.development.port}`
-    if (typeof window.web3 !== 'undefined') {
-        console.log('Web3 detected on window')
-        // Use Mist/MetaMask's provider
-        window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-        console.log('No Web3 detected \nSet Web3')
-        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-        window.web3 = new Web3(new Web3.providers.HttpProvider(web3Location))
-        console.log('web3 added to window')
-    }
-    // ======================================================
-    // Prepare smart contracts
-    // ======================================================
-    const {Dao1901Members, Dao1901Votes, Owned} = Dao1901Contracts
-    Promise.all([Dao1901Members, Dao1901Votes, Owned]
-        .map((contract) => {
-            contract.setProvider(window.web3.currentProvider)
-            return contract.deployed()
-        }))
-        .then((deployedContracts) => {
-            deployedContracts.forEach(function (contract) {
-                // Add name property to the object
-                let name = contract.constructor.contract_name
-                contract['name'] = name
-                // Add each contract to exported contracts
-                contracts[name] = contract
-            })
-            console.log('Smart contracts ready')
-        })
-        .then(() => {
-            // ======================================================
-            // Run Sagas
-            // ======================================================
-            sagaMiddleware.run(rootSaga)
-        })
+  // Set Web3
+  let web3Location = `http://${truffleConfig.networks.development.host}:${truffleConfig.networks.development.port}`
+  if (typeof window.web3 !== 'undefined') {
+    console.log('Web3 detected on window')
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(window.web3.currentProvider)
+  }
+  else {
+    console.log('No Web3 detected \nSet Web3')
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    window.web3 = new Web3(new Web3.providers.HttpProvider(web3Location))
+    console.log('web3 added to window')
+  }
+  // ======================================================
+  // Prepare smart contracts
+  // ======================================================
+  const {Dao1901Members, Dao1901Votes, Owned} = Dao1901Contracts
+  Promise.all([Dao1901Members, Dao1901Votes, Owned]
+    .map((contract) => {
+      contract.setProvider(window.web3.currentProvider)
+      return contract.deployed()
+    }))
+    .then((deployedContracts) => {
+      deployedContracts.forEach(function (contract) {
+        // Add name property to the object
+        let name = contract.constructor.contract_name
+        contract['name'] = name
+        // Add each contract to exported contracts
+        contracts[name] = contract
+      })
+      console.log('Smart contracts ready')
+    })
+    .then(() => {
+      // ======================================================
+      // Run Sagas
+      // ======================================================
+      sagaMiddleware.run(rootSaga)
+    })
 })
 export default store
