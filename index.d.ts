3aa09787ae44d58a5ba4fc19b4123310a2165738
Janaka-Steph <rochestephane@yahoo.fr>
2017-08-08 18:12:28 +0200

    fix ts type declaration

--- index.d.ts
+++ index.d.ts
@@ -5,10 +5,12 @@ declare module "*.json" ;
 
 interface Window {
   web3: any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
 }
 
+declare var Promise: any;
 // Truffle injects the following into the global scope
-declare var web3: any; // TODO: figure out how to use Web3 definition from within global.d.ts instead of `any`
+declare var Web3: any; // TODO: figure out how to use Web3 definition from within global.d.ts instead of `any`
 declare var artifacts: any;
 declare var contract: any;
 declare var before: any;
